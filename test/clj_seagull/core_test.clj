76d2906044299432e1c625b27efe86482b2c6b04
JT W <jwolohan@gmail.com>
2018-09-29 12:23:05 -0400

    Added cooccurrence test

--- test/clj_seagull/core_test.clj
+++ test/clj_seagull/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [clj-seagull.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest cooccurrences-test
+  (testing "Tokens to occurances."
+    (is (= 
+        (cooccurrences ["a" "b" "c"])
+        {"a" {"b" 1 "c" 1} "b" {"c" 1} "c" {}}))))
