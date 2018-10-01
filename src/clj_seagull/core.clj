7408b6be66f6d654ca04ec6e7ee0b190c1b01ce9
JT W <jwolohan@gmail.com>
2018-10-01 07:06:02 -0400

    Improve documentation

--- src/clj_seagull/core.clj
+++ src/clj_seagull/core.clj
@@ -5,17 +5,35 @@
   (:gen-class)))
 
 (defn cooccurrences
-  "Get coccurrences"
+  "Get coccurrences from tokenized text.
+  
+  Though intended for words, `xs` can be any list. Because this function is
+  designed to be input into a bi-directional graph, no effort is made to
+  maintain symmetry.
+
+  Example: 
+  (cooccurrences [\"fly\" \"my\" \"pretties\"])
+  >>> {\"fly\" {\"my\" 1, \"pretties\" 1},
+       \"my\" {\"pretties\" 1}, \"pretties\" {}}   "
   [xs]
   (loop [acc {} 
          xs xs]
     (if (empty? xs)
       acc
       (recur 
-        (update acc (first xs) (fn [m] (merge-with + m (frequencies (take 5 (rest xs))))))
+        (update acc 
+               (first xs) 
+               (fn [m] (merge-with + m (frequencies (take 5 (rest xs))))))
         (rest xs)))))
        
 (defn txt-to-lex-graph
+  "Turns a string of text into a weighted graph of tokens.
+  
+  The sum of the coccurrence counts will be the weight of a connection, so
+  the more often a word occurs next to another, the strong the tie between
+  the two.
+  
+  The return object will be a `loom.graph/weighted-graph`."
   [txt]
   (loom/weighted-graph
   (reduce 
@@ -27,28 +45,56 @@
       (str/split txt #"[.?!]")))))
 
 (defn weighted-next-steps
-  "Returns a weighted sequence of possible next steps"
+  "Returns a weighted sequence of possible next steps.
+  
+  Finds all possible nodes connecting to node `n` on graph `g` and 
+  returns a sequence of these nodes, with each node occurring a number
+  of times equal to the weight of the edge between the nodes.
+  
+  For example, if node 'a' is connected to 'b' with weight 2 and 'c' 
+  with weight 1, then the resulting sequence would be: ['b' 'b' 'c']"
   [g n]
   (reduce (fn [acc [n1 n2]] (into acc (repeat (loom.graph/weight g n1 n2) n2))) [] (loom.graph/out-edges g n)))
   
 (defn random-walk
-  "Randomly walk graph and return the path"
+  "Return a randomly walked path on graph `g` from a `start` node.
+  
+  Starting from the node `start` on graph `g` and continuing until `steps`
+  number of steps have been taken, `random-walk` will continue along the
+  graph by randomly selecting a neighboring node. The probability of
+  a neighboring node being selected is equal to the ratio of the  weight 
+  of the edge between the current node and that node and the sum of the
+  edge weights for the current node. For example, if we are currently on 
+  node 'a', which has edges between 'b' (weight 3) and c (weight 1), the
+  probability fo moving to b is 3/(3+1).
+
+  The returned object is a `steps` length sequence containing the path
+  of the random walk."
   [g start steps]
-  ;; Get random nth from sequence of n possible edges
-  ;; something like: (rand-nth (repeat edges by weight))
   (loop [pth [start]]
     (if (= (count pth) steps) pth
       (recur (conj pth (rand-nth (weighted-next-steps g (first pth))))))))
 
 (defn seeded-walk-freqs
-  "Get the frequency with which words appear in a number of random walks."
+  "Get the frequency with which words appear in a number of random walks.
+  
+  Performs a number of random `walks` along a given weighted-graph `g`.
+  The random walks may occur from one of several starting points,
+  or `seeds`. The total number of walks will be equal to 
+  `walks` times (count `seeds`). The total number of steps taken will 
+  equal `steps` times that number.
+
+  Each walk is taken independent of previous walks.
+
+  The frequency at which nodes were visited on all the random walks is returned."
   [g steps walks seeds]
   (r/fold 
-    (fn ([]{}) ([acc nxt] (merge-with + acc (frequencies (random-walk g nxt steps)))))
+    (fn ([]{}) 
+        ([acc nxt] (merge-with + acc (frequencies (random-walk g nxt steps)))))
     (reduce (fn [acc nxt] (into acc (repeat walks nxt))) [] seeds)))
     
 (defn multiple-seeded-walks
-  "Perform seeded random walks on multiple sets of seeds"
+  "Perform seeded random walks on multiple sets of seed."
   [g steps walks & seeds]
   (map (fn [x] (seeded-walk-freqs g steps walks x)) seeds))
 
@@ -60,6 +106,6 @@
 ))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Empty main function."
   [& args]
   (println "Hello, World!"))
