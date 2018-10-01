dd6df6c5290eb55b1934317fc4b81decd7bab130
JT W <jwolohan@gmail.com>
2018-10-01 19:56:12 -0400

    moved docs

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [aysylu/loom "1.0.2"]]
   :plugins [[lein-codox "0.10.4"]]
+  :codox {:output-path "./docs/"}
   :main ^:skip-aot clj-seagull.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
